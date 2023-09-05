0cefd12daf5b43acc14bc594609d1252c747d727
Maksim <111878146+r-u-f-u-s@users.noreply.github.com>
2023-09-06 01:57:08 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    arr.push(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`);
+  };
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < str.length; j++) {
+      if (`${str[j]}${str[j + 1]}` == arr[i]) {
+        str = str.replace(arr[i], '');
+        i = -1;
+      };
+    };
+  };
+
+  if (str.length == 0) {
+    return true;
+  } else {
+    return false;
+  };
+};
+// идею) решения задачи подсмотрел у https://github.com/skydiver-a https://github.com/skydiver-a/brackets/blob/master/src/index.js
